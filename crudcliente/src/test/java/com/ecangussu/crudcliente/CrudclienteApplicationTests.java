751722e0ef1b1f2ac7847e9c28e9641636eb468e
Estevao Souza <estevaosouza18@gmail.com>
2021-03-01 19:56:08 -0300

    Criação do projeto

---
+++ crudcliente/src/test/java/com/ecangussu/crudcliente/CrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecangussu.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
